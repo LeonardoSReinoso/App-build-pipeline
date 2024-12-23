e50c41086aee8d2303da10b8520c2b70d934f66f
Leonardo Reinoso <leo.leomp3@gmail.com>
2024-12-23 11:54:52 -0300

    update

--- app/SampleApp.py
+++ app/SampleApp.py
@@ -1,32 +1,10 @@
-import json
-from flask import Flask, jsonify, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def home():
-    message = {
-        "status": "success",
-        "message": "Hello, this is a sample JSON response!",
-        "data": {
-            "app": "Sample Python App",
-            "version": "1.0",
-            "author": "Leonardo"
-        }
-    }
-    return jsonify(message), 200
-
-@app.route("/send", methods=["POST"])
-def send_message():
-    input_data = request.get_json()
-    if not input_data:
-        return jsonify({"error": "No JSON data received"}), 400
-
-    response = {
-        "status": "success",
-        "received": input_data
-    }
-    return jsonify(response), 200
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app!!!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
